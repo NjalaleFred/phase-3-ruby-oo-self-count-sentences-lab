99f75a38473a1ba864c5bbe8c931c194390eb093
Njalale <fnjalale@gmail.com>
2023-05-19 17:48:31 +0300

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    count_sentence = self.split(/[.?!]/)
+    sentence_count = count_sentence.reject! {|string| string.empty?}
 
+  if sentence_count == true
+    sentence_count.count
+  else
+    count_sentence.count
+  end
   end
 end
